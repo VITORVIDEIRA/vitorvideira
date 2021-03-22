e321c6a40cb6cd079b9c64ad831b2f408399c0b3
Vitor Videira <vitorvideira95@gmail.com>
2021-03-22 14:34:18 +0000

    images home

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -3,8 +3,15 @@
 import './clock';
 
 import '../css/scss/main.scss';
+
 // Images.
 import '../img/vitorvideira.jpg';
+import '../img/red-arrow.png';
+
+import '../img/linkedin.svg';
+import '../img/facebook.svg';
+import '../img/github.svg';
+import '../img/instagram.svg';
 
 //scripts
 import toggleHamburger from './scripts/hamburger-menu';
